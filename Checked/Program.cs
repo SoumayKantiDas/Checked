5bfd46fe88050e1e0ce796fad2db70dc37938737
Soumay kanti Das <anikds2859@outlook.com>
2021-06-03 13:10:18 +0600

    Add project files.

---
+++ Checked/Program.cs
@@ -0,0 +1,14 @@
+﻿//st
+using System;
+
+namespace Checked
+{
+    class Program
+    {
+        static void Main()
+        {
+             OverflowTest.CheakedMethod();
+            Console.WriteLine("Cheked output value is:{0}", CheackedMethod());
+        }
+    }
+}
---
+++ Checked/overflowTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checked
+{
+    class overflowTest
+    {
+        static short x = 32767;   //Max short value
+        static short y = 32767;
+
+        // Using a checkedMethod
+        static int Checkedmethod()
+        {
+            int z = 0;
+            try
+            {
+                z = checked((short)(x + y));
+            }
+            catch(System.OverflowException e)
+            {
+                Console.WriteLine(e.ToString());
+
+            }
+            return z;
+        }
+
+    }
+}
